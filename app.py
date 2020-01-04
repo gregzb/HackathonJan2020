eee5f27c1ef520959d60f088d6ae9a448d1d6038
Greg Zborovsky <greg.zbo3@gmail.com>
2020-01-04 09:54:12 -0500

    edited template

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-# Jackson Zou
-# SoftDev1 PD 9
-# K
-#
-
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -11,8 +6,9 @@ from flask import url_for
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("template.html")
+@app.route("/index")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.debug = True
