745804006d36531b524a004e68e1ac08d5b87441
Greg Zborovsky <greg.zbo3@gmail.com>
2020-01-04 18:12:04 -0500

    pisdbfijs

--- static/js/base.js
+++ static/js/base.js
@@ -15,39 +15,39 @@ jQuery(function($) {
     });
   });
 
-  $('.title-input').each((idx, el) => {
-    el.addEventListener('focusout', (e) => {
-      console.log("Title was set");
-      console.log(e);
-    });
-  });
+  // $('.title-input').each((idx, el) => {
+  //   el.addEventListener('focusout', (e) => {
+  //     //console.log("Title was set");
+  //     //console.log(e);
+  //   });
+  // });
 
   let handle_focus_out = (el, e) => {
     let parent = e.target.parentElement.parentElement.parentElement.parentElement.parentElement;
 
     let anyEmpty = false;
 
-    console.log("childreN!");
-    console.log(parent);
+    //console.log("childreN!");
+    //console.log(parent);
 
     $(parent).children().each((idx, item) => {
-      console.log("thingy: ");
-      console.log(item);
+      //console.log("thingy: ");
+      //console.log(item);
       let input_param = $(item).children()[0];
       //input_param = $(input_param).children()[0];
       input_param = $(input_param).children()[1];
       input_param = $(input_param).children()[0];
       input_param = $(input_param).children()[0];
       let text = $(input_param).val();
-      console.log(text);
+      //console.log(text);
       if ( text.length == 0) {
         anyEmpty = true;
       }
     });
 
     if (anyEmpty == false) {
-      console.log("parent: ~~~~~~");
-      console.log(parent);
+      //console.log("parent: ~~~~~~");
+      //console.log(parent);
       let new_one = $(e.target.parentElement.parentElement.parentElement.parentElement).clone(false);
       new_one[0].addEventListener('focusout', (e) => {
         handle_focus_out(el, e);
@@ -64,32 +64,35 @@ jQuery(function($) {
   $('.todo-input').each((idx, el) => {
     el.addEventListener('focusout', (e) => {
       handle_focus_out(el, e);
-      // let parent = e.target.parentElement.parentElement.parentElement.parentElement.parentElement;
-      //
-      // let anyEmpty = false;
-      //
-      // $(parent).each((idx, item) => {
-      //   let input_param = $(item).children()[0];
-      //   input_param = $(input_param).children()[0];
-      //   input_param = $(input_param).children()[1];
-      //   input_param = $(input_param).children()[0];
-      //   input_param = $(input_param).children()[0];
-      //   let text = $(input_param).val();
-      //   if ( text.length == 0) {
-      //     anyEmpty = true;
-      //   }
-      // });
-      //
-      // if (anyEmpty == false) {
-      //   let new_one = $(e.target.parentElement.parentElement.parentElement.parentElement).clone();
-      //   let child = $(new_one).children()[0];
-      //   child = $(child).children()[1];
-      //   child = $(child).children()[0];
-      //   child = $(child).children()[0];
-      //   $(child).val("");
-      //   $(parent).append(new_one);
-      // }
-
     });
+  });
+
+  $('#new-list')[0].addEventListener('click', (e) => {
+    let copy = $($("#todolists").children()[0]).clone(false);
+    copy.css("width", "auto");
+    copy.css("height", "auto");
+    copy.css("visibility", "visible");
+    copy.find('.todo-input').each((idx, el) => {
+      el.addEventListener('focusout', (e) => {
+        handle_focus_out(el, e);
+      });
+    });
+    copy.find('.todo-input').each((idx, el) => {
+      el.addEventListener('focusout', (e) => {
+        handle_focus_out(el, e);
+      });
+    });
+    copy.find('.del').each((idx, el) => {
+      el.addEventListener('click', (e) => {
+        let parent = e.target.parentElement.parentElement.parentElement.parentElement;
+        console.log("found " + parent.childElementCount);
+        if (parent.childElementCount > 1) {
+          $(parent).remove();
+        }
+        //4 levels of parent
+      });
+    });
+    $("#todolists").append($.parseHTML("<br>"));
+    $("#todolists").append(copy);
   });
 });
